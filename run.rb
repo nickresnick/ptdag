5a3d735998c1644b68b48f25b3f0ac20a25aebb3
Nick Resnick <nick@custora.com>
2017-12-09 17:13:05 -0500

    style tweak

--- run.rb
+++ run.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 require './pivotal_dag'
 config = YAML.load_file('./config.yml')
+
 dag = PivotalDag.new(
   api_key: config['PivotalSettings']['APIKey'],
   project_id: config['PivotalSettings']['ProjectID'],
   epic_name: config['PivotalSettings']['EpicName'],
   include_key: config['GraphSettings']['IncludeKey'],
 )
+
 dag.generate_dag(
   config['OutputSettings']['FileFormat'],
   config['OutputSettings']['FilePath'],
